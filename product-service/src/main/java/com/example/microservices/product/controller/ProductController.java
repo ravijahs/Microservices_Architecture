dc2ae98f662815891366b13dd7ee5246c555589b
Ravija <ravijasalpitikorala@gmail.com>
2025-04-21 22:10:37 +0530

    first commit

---
+++ product-service/src/main/java/com/example/microservices/product/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.example.microservices.product.controller;
+
+import com.example.microservices.product.dto.ProductRequest;
+import com.example.microservices.product.dto.ProductResponse;
+import com.example.microservices.product.model.Product;
+import com.example.microservices.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
+        return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
---
+++ product-service/src/main/java/com/example/microservices/product/service/ProductService.java
@@ -0,0 +1,34 @@
+package com.example.microservices.product.service;
+
+import com.example.microservices.product.dto.ProductRequest;
+import com.example.microservices.product.dto.ProductResponse;
+import com.example.microservices.product.model.Product;
+import com.example.microservices.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+        productRepository.save(product);
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice()))
+                .toList();
+    }
+}
