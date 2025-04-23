84beb13f933aec7b92efda0c62b06f27ac590f29
Ravija <ravijasalpitikorala@gmail.com>
2025-04-23 23:29:00 +0530

    creating endpoints in the inventory service and the configuration

---
+++ inventory-service/src/main/java/com/example/microservices/inventory/controller/InventoryController.java
@@ -0,0 +1,20 @@
+package com.example.microservices.inventory.controller;
+
+import com.example.microservices.inventory.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/inventory")
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity) {
+        return inventoryService.isInStock(skuCode, quantity);
+    }
+}
---
+++ inventory-service/src/main/resources/db/migration/V2_init.sql
@@ -0,0 +1,4 @@
+INSERT INTO t_inventory (quantity, sku_code)
+values (100, 'iPhone_15'),
+       (100, 'pixel_8'),
+       (100, 'galaxy_S24');
