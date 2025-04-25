bb2a1c166a0d7551ecaa760c7c3fad8ca89772f4
Ravija <ravijasalpitikorala@gmail.com>
2025-04-26 00:18:49 +0530

    calling the inventry API from the order-service

--- order-service/src/main/java/com/example/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/example/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.example.microservices.order.service;
 
+import com.example.microservices.order.client.InventoryClient;
 import com.example.microservices.order.dto.OrderRequest;
 import com.example.microservices.order.model.Order;
 import com.example.microservices.order.repository.OrderRepository;
@@ -13,13 +14,22 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderService {
 
+    private final InventoryClient inventoryClient;
     private final OrderRepository orderRepository;
+
     public void placeOrder(OrderRequest orderRequest) {
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+
+        boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with SkuCode" + orderRequest.skuCode() + "is not in stock.");
+        }
     }
 }
