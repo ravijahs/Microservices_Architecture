098d7f7ae571d0deaf826427c7082f4f1d4b8d48
Ravija <ravijasalpitikorala@gmail.com>
2025-05-18 00:12:45 +0530

    minor changes in product test

--- product-service/src/test/java/com/example/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/example/microservices/product/ProductServiceApplicationTests.java
@@ -1,6 +1,7 @@
 package com.example.microservices.product;
 
 import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,7 +9,6 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MongoDBContainer;
-import org.testcontainers.shaded.org.hamcrest.Matchers;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -47,10 +47,10 @@ class ProductServiceApplicationTests {
                 .post("/api/product")
                 .then()
                 .statusCode(201)
-                .body('id', Matchers.notNullValue())
-                .body('name', Matchers.equalTo("Iphone 15"))
-                .body('description', Matchers.equalTo("Smart phone from Apple"))
-                .body('price', Matchers.equalTo(1000));
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("Iphone 15"))
+                .body("description", Matchers.equalTo("Smart phone from Apple"))
+                .body("price", Matchers.equalTo(1000));
     }
 
 }
