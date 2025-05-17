022988c61b831b2d6fc24e652375ccbff7249eda
Ravija <ravijasalpitikorala@gmail.com>
2025-05-18 00:49:55 +0530

    minor changes

--- order-service/src/main/java/com/example/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/example/microservices/order/client/InventoryClient.java
@@ -1,14 +1,11 @@
 package com.example.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(value = "inventory", url = "http://localhost:8082")
 public interface InventoryClient {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    @GetExchange("inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
 }
